8c8dd9a6c62a6ca6ca694e8d68765e8b29dd3ea4
Nik <skitchen1992@gmail.com>
2021-07-12 19:47:43 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import Nav from "./components/Nav/Nav";
 import './App.module.css';
 import {Route, withRouter} from "react-router";
 import classes from "./App.module.css";
-//import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-//import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import {connect} from "react-redux";
@@ -45,9 +43,6 @@ class App extends Component<AppPropsType> {
                 <div className={classes.content}>
                     <Route path="/dialogs" render= {WithSuspense(DialogsContainer)}/>
                     <Route path="/profile/:userId?" render={WithSuspense(ProfileContainer)}/>
-
-                    {/*<Route path="/dialogs" render={() => <DialogsContainer/>}/>*/}
-                    {/*<Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>*/}
                     <Route path="/users" render={() => <UsersContainer/>}/>
                     <Route path="/login" render={() => <LoginPage/>}/>
                 </div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,17 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 import store from "./redux/redux-store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import App from "./App";
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
